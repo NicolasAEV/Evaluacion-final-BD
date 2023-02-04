81515987e9a4377f742cc7329f84873949a9cff7
NicolasAEV <NicolasEscobar.v@hotmail.com>
2023-02-04 14:11:40 -0300

    se poblaron las nuevas tablas de datos actor, reparto actorer y teleseries y se realizo la ultima consulta requerida

--- Evaluacion.sql
+++ Evaluacion.sql
@@ -1,25 +1,30 @@
 --1 Crear una consulta para obtener todos los actores que participaron en ambas teleseries,
 --el sueldo que obtuvieron en cada una y la suma de ambos sueldos, todo esto ordenado por el name
 --del actor
-SELECT R.nombre , R.sueldo, S.sueldo,(R.sueldo + S.sueldo )AS sueldoTotal
+SELECT R.nombre , R.sueldo AS Sueldo_Papi_ricky, S.sueldo AS Sueldo_Soltera_Otra_vez,(R.sueldo + S.sueldo )AS sueldoTotal
 FROM reparto_papi_ricky AS R
 INNER JOIN reparto_soltera_otra_vez AS S
 ON R.nombre = S.nombre
 ORDER BY R.nombre ASC;
+
 --2 Crear una consulta para obtener solo los actores que participaron exclusivamente en soltera otra vez 
 -- con un sueldo mayor a 90 RIGHT OUTER JOIN
 --SELECT * FROM reparto_soltera_otra_vez WHERE sueldo > 90;
-SELECT S.nombre, S.sueldo 
-FROM reparto_soltera_otra_vez AS S
-LEFT OUTER JOIN reparto_papi_ricky AS R
-ON R.nombre = S.nombre
-WHERE S.sueldo > 90;
+
+SELECT S.nombre AS "reparto soltera otra vez",R.nombre AS "reparto papi ricky", S.sueldo 
+FROM reparto_papi_ricky AS R
+FULL OUTER JOIN  reparto_soltera_otra_vez AS S
+ON R.nombre = S.nombre 
+WHERE R.nombre ISNULL 
+AND S.sueldo > 90;
+
 --3 Crear una consulta para obtener los actores con sueldo inferior a 85 que actuaron en cualquiiera de las dos 
 -- teleseries pero no en las dos CON 
 
-SELECT S.nombre,S.sueldo AS SUELDO1, R.nombre, R.sueldo AS SUELDO2
+SELECT CONCAT (S.nombre, R.nombre)AS NOMBRE,
+CONCAT (S.sueldo, R.sueldo) AS SUELDO
 FROM reparto_soltera_otra_vez AS S
 FULL OUTER JOIN reparto_papi_ricky AS R
-ON R.nombre = S.nombre
+ON R.nombre = S.nombre 
 WHERE (R.nombre = S.nombre IS NULL) 
-AND (S.sueldo < 85 or R.sueldo <85);
+AND (S.sueldo < 85 OR R.sueldo <85);
